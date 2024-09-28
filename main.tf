634d5030afde7c44d97c6fe979f02fa9835933c7
Jonathan Bruno <jonathanbruno.ueg@gmail.com>
2024-09-28 15:34:21 -0300

    save

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ provider "aws" {
 
 data "aws_caller_identity" "current" {}
 
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
+#output "aws_account_id" {
+#  value = data.aws_caller_identity.current.account_id
+#}
